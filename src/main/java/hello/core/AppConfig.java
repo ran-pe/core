89aa5818d35a74e7b9b634fa58c37f098dd69600
youngran <horany83@gmail.com>
2021-03-10 23:36:14 +0900

    2021-03-10

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
+    // AppConfig 호출시 기대
+//    call AppConfig.memberService
+//    call AppConfig.memberRepository
+//    call AppConfig.memberRepository
+//    call AppConfig.orderService
+//    call AppConfig.memberRepository
+
+    // AppConfig 호출시 실제 log
+//    call AppConfig.memberService
+//    call AppConfig.memberRepository
+//    all AppConfig.orderService
+
     @Bean
     public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
